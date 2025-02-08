cad886433406c38ab9bd56a358a054b3c9e6bfa2
codingdaddy-ae <arghadeeptambuli05@gmail.com>
2025-02-09 00:05:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure?",
+    "Really sure tutu??",
+    "Are you corona positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad my tutu putu...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.getElementById("no-button");
+    const yesButton = document.getElementById("yes-button");
+    noButton.innerText = messages[messageIndex];
+    messageIndex++;
+    currentFontSize = parseInt(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize= (currentFontSize * 1.5) + "px" ;
+
+}
+function handleYesClick(){
+    window.location.href = "yes.html";
+}
